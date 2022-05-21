b92a86c264ff6ab487be365a6a0f07fa5c6b877a
FUJIMURA Hidenori <hfu@users.noreply.github.com>
2022-05-21 14:32:31 +0900

    update

--- constants.rb
+++ constants.rb
@@ -1,21 +1,40 @@
-#TXT_PATH = 'all.txt'
-#TXT_PATH = 'fujisan.txt'
-TXT_PATH = 'all.txt'
-DST_DIR = 'dst'
-DZ = 6
-J = 3
-MBTILES_DIR = "mbtiles"
-MINZOOM = 12
-MAXZOOM = 25
-KEI = 8
-SRC_EPSG = "EPSG:#{2442 + KEI}"
-DST_EPSG = "EPSG:4326"
-PORT = 8006
+# G空間情報センターの点群データのURLリストのパス
+TXT_PATH = 'fujisan.txt'
+# obs: 書き出す MBTiles ファイルのパス（非分割の場合）
 MBTILES_PATH = 'tiles.mbtiles'
-URL_PATH = 'blocks'
-ZCAT = `uname`.chop == 'Darwin' ? 'gzcat' : 'zcat'
 
+# txt.gz ファイルを書き出すディレクトリ
+DST_DIR = 'dst'
+# LAS ごとの MBTiles ファイルを書き出すディレクトリ
+MBTILES_DIR = "mbtiles"
+# obs: グループごとにまとめた MBTiles を書き出すディレクトリ
 GROUPS_DIR = 'groups'
+# 一時ファイルのディレクトリ
+TMP_DIR = '/tmp'
+
+# ZFXYのzとそれが格納されるタイルのzの差
+DZ = 6
+# GNU Parallel で並行実行する並行度
+J = 3
+# 生産するタイルの最小ズーム
+MINZOOM = 12
+# 生産するタイルの最大ズーム
+MAXZOOM = 25
+# obs: グループごとに生産する場合のまとめる個数
 GROUP_SIZE = 1024
 
-TMP_DIR = '/tmp'
+# 静岡点群ファイルの平面直角座標系の系
+KEI = 8
+# 静岡点群ファイルのEPSGコード
+SRC_EPSG = "EPSG:#{2442 + KEI}"
+# PDALが書き出すデータのESPGコード
+DST_EPSG = "EPSG:4326"
+
+# タイルをホストするポートの番号
+PORT = 8006
+# URL上のパス
+URL_PATH = 'blocks'
+
+# gzip ファイルを展開するコマンドの名前
+ZCAT = `uname`.chop == 'Darwin' ? 'gzcat' : 'zcat'
+
